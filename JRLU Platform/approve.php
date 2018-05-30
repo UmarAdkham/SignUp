7f3018fd72bc17530c2d399b38b5cae83f33858f
Umar Adkhamov <35759152+UmarAdkham@users.noreply.github.com>
2018-05-31 02:03:35 +0800

    Delete php folders

--- JRLU Platform/approve.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$message = "Application has been approved";
-	echo "<script type='text/javascript'>alert('$message');
-	window.location.href = 'home.php';
-	</script>";
-
-?>
--- JRLU Platform/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION["admin_username"]);
-  unset($_SESSION["admin_firstname"]);
-  unset($_SESSION["admin_lastname"]);
-  unset($_SESSION["bankID"]);
-  header("Location: index.php");
-?>
